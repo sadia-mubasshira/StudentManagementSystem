3436a971f273e3268500af96e0d2d6fa1a19117b
SADIA MOBASSHIRA <120294560+m-snigdha@users.noreply.github.com>
2022-12-11 10:57:12 +0530

    Create studentMgmtDatabaseConn.java

---
+++ studentMgmtDatabaseConn.java
@@ -0,0 +1,36 @@
+//begin imports
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//finished import
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SADIA MOBASSHIRA
+ */
+public class studentMgmtDatabaseConn {
+    final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final static String DB_URL = "jdbc:mysql://localhost:3306/Database Name: Student_Mgmt_Sadia";
+    final static String USER = "root";
+    final static String PASSWORD = "";
+    
+    public static Connection connection(){
+        try{
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+            return conn;
+        }
+
+        catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+    
+}
