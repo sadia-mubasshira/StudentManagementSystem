71194830d6588c4380f4241fb681007f3a3688d0
SADIA MOBASSHIRA <120294560+m-snigdha@users.noreply.github.com>
2022-12-12 20:19:24 +0530

    Update studentMgmtDatabaseConn.java

--- studentMgmtDatabaseConn.java
+++ studentMgmtDatabaseConn.java
@@ -3,7 +3,6 @@ import javax.swing.JOptionPane;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-//finished import
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -13,9 +12,26 @@ import java.sql.SQLException;
 /**
  *
  * @author SADIA MOBASSHIRA
- */
+ * @Department of CSE
+ * @ID: 21-45727-3
+ * @American International University - Bangladesh
+ *
+ **/
+
+/*
+ 
+  ________     __       ________   __          __       ___      ___     ______    _______       __        ________  ________  __    __   __      _______        __      
+ /"       )   /""\     |"      "\ |" \        /""\     |"  \    /"  |   /    " \  |   _  "\     /""\      /"       )/"       )/" |  | "\ |" \    /"      \      /""\     
+(:   \___/   /    \    (.  ___  :)||  |      /    \     \   \  //   |  // ____  \ (. |_)  :)   /    \    (:   \___/(:   \___/(:  (__)  :)||  |  |:        |    /    \    
+ \___  \    /' /\  \   |: \   ) |||:  |     /' /\  \    /\\  \/.    | /  /    ) :)|:     \/   /' /\  \    \___  \   \___  \   \/      \/ |:  |  |_____/   )   /' /\  \   
+  __/  \\  //  __'  \  (| (___\ |||.  |    //  __'  \  |: \.        |(: (____/ // (|  _  \\  //  __'  \    __/  \\   __/  \\  //  __  \\ |.  |   //      /   //  __'  \  
+ /" \   :)/   /  \\  \ |:       :)/\  |\  /   /  \\  \ |.  \    /:  | \        /  |: |_)  :)/   /  \\  \  /" \   :) /" \   :)(:  (  )  :)/\  |\ |:  __   \  /   /  \\  \ 
+(_______/(___/    \___)(________/(__\_|_)(___/    \___)|___|\__/|___|  \"_____/   (_______/(___/    \___)(_______/ (_______/  \__|  |__/(__\_|_)|__|  \___)(___/    \___)
+                                                                                                                                                                         
+*/
+
 public class studentMgmtDatabaseConn {
-    final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     final static String DB_URL = "jdbc:mysql://localhost:3306/student_mgmt_sadia";
     final static String USER = "root";
     final static String PASSWORD = "";
@@ -26,7 +42,6 @@ public class studentMgmtDatabaseConn {
             Connection conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
             return conn;
         }
-
         catch(ClassNotFoundException | SQLException e){
             JOptionPane.showMessageDialog(null, e);
             return null;
